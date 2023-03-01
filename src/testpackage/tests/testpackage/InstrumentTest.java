9bf9e0e7d177c09834107d51941578634e657be2
TaylorMorini <90061586+TaylorMorini@users.noreply.github.com>
2023-03-01 18:24:44 -0500

    Added tests for everything

---
+++ src/testpackage/tests/testpackage/InstrumentTest.java
@@ -0,0 +1,64 @@
+package testpackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InstrumentTest {
+
+    @Test
+    void getInstrumentName() {
+    }
+
+    @Test
+    void setInstrumentName() {
+    }
+
+    @Test
+    void getInstrumentBrand() {
+    }
+
+    @Test
+    void setInstrumentBrand() {
+    }
+
+    @Test
+    void getInstrumentAge() {
+    }
+
+    @Test
+    void setInstrumentAge() {
+    }
+
+    @Test
+    void getInstrumentCond() {
+    }
+
+    @Test
+    void setInstrumentCond() {
+    }
+
+    @Test
+    void getInstrumentType() {
+    }
+
+    @Test
+    void setInstrumentType() {
+    }
+
+    @Test
+    void getInstrumentMat() {
+    }
+
+    @Test
+    void setInstrumentMat() {
+    }
+
+    @Test
+    void getNumStrings() {
+    }
+
+    @Test
+    void setNumStrings() {
+    }
+}
