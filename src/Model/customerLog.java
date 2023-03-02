ea263f59876fb1c62ee6f28d8f05098522d84ee2
TaylorMorini <90061586+TaylorMorini@users.noreply.github.com>
2023-03-02 10:48:40 -0500

    added an idea

---
+++ src/Model/customerLog.java
@@ -0,0 +1,10 @@
+package Model;
+
+/**
+ * May be place to setup database?
+ * Database of customer log?
+ *  Could we have it so that if the customer is already in the system, the database can access t
+ *  their information?
+ */
+public class customerLog {
+}
