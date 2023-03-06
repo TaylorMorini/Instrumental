38196a8fc35b2b747353379d0d60dd287b211eb5
AngelinaIST <afw5426@psu.edu>
2023-03-05 20:53:28 -0500

    Added some listeners for JFrame instrument and transaction

---
+++ src/Controller/instrumentController.java
@@ -0,0 +1,60 @@
+package Controller;
+import java.awt.event.*;
+
+public class instrumentController {
+    class readName implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readBrand implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readAge implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readCond implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readType implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readMat implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readNumStrings implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
--- src/Controller/transactionController.java
+++ src/Controller/transactionController.java
@@ -1,6 +1,31 @@
 package Controller;
 
+import java.awt.event.*;
 
 
 public class transactionController {
+
+    class readName implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readPrice implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class readAddress implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
 }
